d38201226da0195ecfa960527b16eb58781f3ba7
m33i <shionish@gmail.com>
2024-06-15 02:47:25 +0200

    voice detection working and refactor

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# listener
+# how to dependencies 
+$ python3 -m pip install --upgrade pip
+$ git clone https://github.com/Rapptz/discord.py
+$ cd discord.py
+$ python3 -m pip install -U .[voice]
+$ python -m pip install discord-ext-voice-recv
--- discord_integration.py
+++ discord_integration.py
@@ -6,34 +6,19 @@ discord.opus._load_default()
 
 bot = commands.Bot(command_prefix=commands.when_mentioned, intents=discord.Intents.all())
 
-class Main(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-
-    @commands.command()
-    async def test(self, ctx):
-        print(f"test")
-        def callback(user, data: voice_recv.VoiceData):
-            print(f"Got packet from {user}")
-
-            # voice power level, how loud the user is speaking
-            ext_data = data.extension_data.get(voice_recv.ExtensionID.audio_power)
-            value = int.from_bytes(ext_data, 'big')
-            power = 127 - (value & 127)
-            print('#' * int(power * (79 / 128)))
-            # instead of 79 you can use shutil.get_terminal_size().columns-1
-
-        vc = await ctx.author.voice.channel.connect(cls=voice_recv.VoiceRecvClient)
-        vc.listen(voice_recv.BasicSink(callback))
+def voice_callback(user, data: voice_recv.VoiceData):
+    print(f"got packet from {user}")
 
 @bot.event
 async def on_ready():
-    print(f"Logged in as {bot.user}")
-    channel = bot.get_channel(1250016469237108748) # wkh
-    await channel.connect()
-    await bot.change_presence(status=discord.Status.online)
-
-async def setup_hook():
-    await bot.add_cog(Main(bot))
+    print(f"logged in as {bot.user}")
+    channel = bot.get_channel(1250016469237108748)  # Reemplaza con tu ID de canal de voz
+    if channel and isinstance(channel, discord.VoiceChannel):
+        vc = await channel.connect(cls=voice_recv.VoiceRecvClient)
+        vc.listen(voice_recv.BasicSink(voice_callback))
+        print(f"connected to voice channel: {channel.name}")
+        await bot.change_presence(status=discord.Status.online)
+    else:
+        print("not found")
 
 bot.run(token)
