2f95c0556795a058d68f7884d5087a1ee7478fb1
m33i <shionish@gmail.com>
2024-06-15 03:45:31 +0200

    mp3 with less bitrate

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ $ git clone https://github.com/Rapptz/discord.py
 $ cd discord.py
 $ python3 -m pip install -U .[voice]
 $ python -m pip install discord-ext-voice-recv
+
+
+pip3 install ffmpeg
--- discord_integration.py
+++ discord_integration.py
@@ -1,7 +1,7 @@
 import discord
 from secret import token
 from discord.ext import commands, voice_recv
-from datetime import date
+import datetime
 import ffmpeg
 import asyncio
 
@@ -13,35 +13,36 @@ discord.opus._load_default()
 bot = commands.Bot(command_prefix=commands.when_mentioned, intents=discord.Intents.all())
 
 def voice_callback(user, data: voice_recv.VoiceData):
-    #print(f"got packet from {user}")
-    #rint(date.isoformat(data.packet.timestamp))
-    payload = data.pcm
+    print(f"got packet from {user} at {datetime.datetime.now()}")
     with open(pcm_file_path, "ab") as pcm_file:
-        pcm_file.write(payload)
+        pcm_file.write(data.pcm)
 
 @bot.event
 async def on_ready():
     print(f"logged in as {bot.user}")
-    channel = bot.get_channel(1250016469237108748)  # Reemplaza con tu ID de canal de voz
+    channel = bot.get_channel(1250016469237108748)  # channel id
     if channel and isinstance(channel, discord.VoiceChannel):
+
         vc = await channel.connect(cls=voice_recv.VoiceRecvClient)
-        vc.listen(voice_recv.BasicSink(voice_callback))
-        print(f"connected to voice channel: {channel.name}")
         await bot.change_presence(status=discord.Status.online)
-        bot.loop.create_task(save_audio_periodically())
+
+        print(f"connected to voice channel: {channel.name}")
+        vc.listen(voice_recv.BasicSink(voice_callback))
+        bot.loop.create_task(save_audio())
     else:
         print("not found")
 
-async def save_audio_periodically():
+# pcm to mp3
+async def save_audio():
     while True:
         await asyncio.sleep(10)
         convert_pcm_to_mp3()
 
 def convert_pcm_to_mp3():
     try:
-        ffmpeg.input(pcm_file_path, format='s16le', ar='48000', ac='2').output(mp3_file_path).run()
-        print(f"Audio guardado y convertido a {mp3_file_path}")
+        ffmpeg.input(pcm_file_path, format='s16le', ar='48000', ac='2').output(mp3_file_path, audio_bitrate='64k').run()
+        print(f"converted to {mp3_file_path}")
     except Exception as e:
-        print(f"Error al convertir el audio: {e}")
+        print(f"conversion error: {e}")
 
 bot.run(token)
